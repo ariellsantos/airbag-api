cc2cacd9661db538e36222fd707ebbd736e57623
Ariel Santos <ariel.easc@gmail.com>
2023-11-08 16:11:48 -0600

    mariadb connection factory and tests to cennection added

--- src/Application/dependency-injection/container.ts
+++ src/Application/dependency-injection/container.ts
@@ -1,6 +1,8 @@
 import { aliasTo, asFunction, createContainer, InjectionMode } from 'awilix';
 import { winstonLogger } from '../winstonLogger';
 import { configFactory } from '../configs/configFactory';
+import { typeormConnectionFactory } from '../../contexts/common/infrastructure/persistence/typeorm/typeormConnectionFactory';
+import { mariadbTypeormConnectionFactory } from '../../contexts/common/infrastructure/persistence/typeorm/mariadb/mariadbConnectionFactory';
 
 const container = createContainer({
   injectionMode: InjectionMode.CLASSIC
@@ -11,7 +13,13 @@ container.register({
   logger: aliasTo('winstonLogger'),
   applicationConfig: asFunction(configFactory, {
     injector: () => ({ resource: 'application' })
-  })
+  }),
+  mariadbConfig: asFunction(configFactory, {
+    injector: () => ({ resource: 'mariadbTypeorm' })
+  }),
+  typeormConnectionFactory: asFunction(typeormConnectionFactory),
+  orm: aliasTo('typeormConnectionFactory'),
+  mariadbConnection: asFunction(mariadbTypeormConnectionFactory)
 });
 
 export { container };
---
+++ src/contexts/common/infrastructure/persistence/typeorm/TypeOrmConfig.ts
@@ -0,0 +1,11 @@
+import DataSourceOptions from 'typeorm';
+
+export type databaseType = 'mysql' | 'mariadb';
+export interface TypeOrmConfig {
+  type: databaseType;
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+}
