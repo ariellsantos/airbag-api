f379df7f1d61bad414f2d2c739c470c73bdb09ab
Ariel Santos <ariel.easc@gmail.com>
2023-11-12 17:35:22 -0600

    job for get and insert latest currencies rates added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,14 @@
-module.exports = [{
-    "script": './dist/src/Application/app.js',
-    "name": "API airbag",
-    "instances": 1,
-    "exec_mode": "cluster"
-}]
+module.exports = [
+    {
+        "script": './dist/src/Application/app.js',
+        "name": "API airbag",
+        "instances": 1,
+        "exec_mode": "cluster"
+    },
+    {
+        "script": './dist/src/Application/jobs/CurrencyRates/insertCurrentCurrenciesRate.job.js',
+        "name": "Job.latest.currencies-rates",
+        "instances": 1,
+        "exec_mode": "cluster"
+    },
+]
